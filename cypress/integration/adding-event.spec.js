9c133149f5e161940a5240dc64ca25db50b520cd
samuelvitry <samuel.vitry@orange.fr>
2022-02-09 11:01:50 +0100

    user can't add incorrect events

--- cypress/integration/adding-event.spec.js
+++ cypress/integration/adding-event.spec.js
@@ -15,6 +15,34 @@ function login() {
         }
     });
 }
+function toHtmlDate(dte, fll) {
+    var flDay;
+    if (fll !== undefined) {
+        flDay = fll;
+    } else {
+        flDay = false;
+    }
+    var date = new Date(dte);
+    let tempM = date.getMonth() + 1;
+    if (tempM < 10) {
+        tempM = "0" + tempM;
+    }
+    let tempD = date.getDate();
+    if (tempD < 10) {
+        tempD = "0" + tempD;
+    }
+    let tempH = date.getHours();
+    if (tempH < 10) {
+        tempH = "0" + tempH;
+    }
+    let temp;
+    if (flDay) {
+        temp = date.getFullYear() + "-" + tempM + "-" + tempD;
+    } else {
+        temp = date.getFullYear() + "-" + tempM + "-" + tempD + "T" + tempH + ":00";
+    }
+    return temp;
+}
 
 describe("adding event", () => {
     /*it("user can add event via the button", () => {
@@ -90,11 +118,22 @@ describe("adding event", () => {
         cy.wait(500);
         cy.get("@dayCard").should("not.contain", "test event");
     });*/
-    it("user cant add incorrect events (empty, end after start)", () => {
+    it("user cant add incorrect events (empty, end after start, end and start at the same time)", () => {
         login();
         cy.get(".monthly-top-button button").click();
         cy.get(".add-button-line .button-full").click();
         cy.get(".add-error-line").should("contain", "Please provide a name");
-        cy.get(":nth-child(2) > .input-open").click();
+
+        cy.get("input[placeholder='Event name']").type("test event");
+
+        cy.get(":nth-child(2) > .input-open").type(toHtmlDate(new Date(new Date().getTime() - 86400000)));
+        cy.get(".add-button-line .button-full").click();
+        cy.get(".add-error-line").should("contain", "Event can't end before starting");
+
+        let dte = toHtmlDate(new Date());
+        cy.get(":nth-child(5) > :nth-child(1) > .input-open").type(dte);
+        cy.get(":nth-child(2) > .input-open").type(dte);
+        cy.get(".add-button-line .button-full").click();
+        cy.get(".add-error-line").should("contain", "Event can't end before starting");
     });
 });
