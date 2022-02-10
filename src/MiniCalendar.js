a227b48c53c78b96cf61cda131b23a3e6387306f
samuelvitry <samuel.vitry@orange.fr>
2022-02-10 14:48:19 +0100

    tests of the mini calendar

--- src/MiniCalendar.js
+++ src/MiniCalendar.js
@@ -9,15 +9,7 @@ export const MiniCalendar = (props) => {
         // January 4 is always in week 1.
         var week1 = new Date(date.getFullYear(), 0, 4);
         // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-        return (
-            1 +
-            Math.round(
-                ((date.getTime() - week1.getTime()) / 86400000 -
-                    3 +
-                    ((week1.getDay() + 6) % 7)) /
-                    7
-            )
-        );
+        return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
     }
 
     function dispatchEvent(nbr) {
@@ -27,42 +19,20 @@ export const MiniCalendar = (props) => {
             offset = 7;
         }
         offset = offset - 1;
-        let start_date =
-            new Date(props.year, props.month - 1, nbr - offset).getTime() /
-            1000;
-        let end_date =
-            new Date(
-                props.year,
-                props.month - 1,
-                nbr - offset,
-                23,
-                59,
-                59
-            ).getTime() / 1000;
+        let start_date = new Date(props.year, props.month - 1, nbr - offset).getTime() / 1000;
+        let end_date = new Date(props.year, props.month - 1, nbr - offset, 23, 59, 59).getTime() / 1000;
         let tempStockage = [];
         for (let i = 0; i < eventList.length; i++) {
             let event = eventList[i];
-            //event['blank'] = false   11/06/2021 don't know why this is here...
-            if (
-                event["start_date"] >= start_date &&
-                event["start_date"] <= end_date
-            ) {
-                tempStockage.push(event);
-            } else if (
-                event["end_date"] >= start_date &&
-                event["end_date"] <= end_date
-            ) {
-                tempStockage.push(event);
-            } else if (
-                event["start_date"] <= start_date &&
-                event["end_date"] >= end_date
-            ) {
-                tempStockage.push(event);
+            if (event["blank"] !== true) {
+                if (event["start_date"] >= start_date && event["start_date"] <= end_date) {
+                    tempStockage.push(event);
+                } else if (event["end_date"] >= start_date && event["end_date"] <= end_date) {
+                    tempStockage.push(event);
+                } else if (event["start_date"] <= start_date && event["end_date"] >= end_date) {
+                    tempStockage.push(event);
+                }
             }
-        }
-
-        while (tempStockage.length < 2) {
-            tempStockage.push({ blank: true });
         }
 
         return tempStockage;
@@ -114,30 +84,21 @@ export const MiniCalendar = (props) => {
         var nbrDot = events.length;
 
         return (
-            <div
-                className={selected ? "mini-jour mini-selected" : "mini-jour"}
-                style={{ color: gris ? "#49505550" : "#111B22" }}>
-                <p
-                    className={
-                        events[0]["blank"] === true &&
-                        events[1]["blank"] === true
-                            ? "mini-none-p"
-                            : null
-                    }>
+            <div className={selected ? "mini-jour mini-selected" : "mini-jour"} style={{ color: gris ? "#49505550" : "#111B22" }}>
+                <p className={events.length <= 0 ? "mini-none-p" : null} style={{ transform: "translateY(0)" }}>
                     {getJour(props.day).getDate()}
                 </p>
-                <div className='mini-dot'>
-                    {nbrDot > 0 ? (
+                <div className="mini-dot">
+                    {events[0] !== undefined ? (
                         <div
-                            className='mini-dot1'
+                            className="mini-dot1"
                             style={{
                                 backgroundColor: events[0]["color"],
                             }}></div>
                     ) : null}
-                    {events[0]["blank"] === false &&
-                    events[1]["blank"] === false ? (
+                    {events[1] !== undefined ? (
                         <div
-                            className='mini-dot2'
+                            className="mini-dot2"
                             style={{
                                 backgroundColor: events[1]["color"],
                             }}></div>
@@ -149,57 +110,20 @@ export const MiniCalendar = (props) => {
 
     const Line = (props) => {
         var classNom = props.annim + " mini-line ";
-        var thisW = getWeek(
-            new Date(props.year, props.month - 1, props.offset + 1)
-        );
+        var thisW = getWeek(new Date(props.year, props.month - 1, props.offset + 1));
         if (thisW === props.nowWeek && props.sele) {
             classNom = classNom.concat("mini-line-sele");
         }
 
         return (
             <div className={classNom}>
-                <MiniJour
-                    month={props.month}
-                    day={1 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={2 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={3 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={4 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={5 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={6 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
-                <MiniJour
-                    month={props.month}
-                    day={7 + props.offset}
-                    premier={props.premier}
-                    dernier={props.dernier}
-                />
+                <MiniJour month={props.month} day={1 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={2 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={3 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={4 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={5 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={6 + props.offset} premier={props.premier} dernier={props.dernier} />
+                <MiniJour month={props.month} day={7 + props.offset} premier={props.premier} dernier={props.dernier} />
             </div>
         );
     };
@@ -219,74 +143,25 @@ export const MiniCalendar = (props) => {
     };
 
     return (
-        <div className='mini-calendar'>
-            <div className='mini-top'>
+        <div className="mini-calendar">
+            <div className="mini-top">
                 <h2>
                     {monthConv[props.month]} {props.year}{" "}
                 </h2>
-                <div className='mini-nav'>
-                    <h2
-                        className='mini-nav-prev'
-                        onClick={() => props.prevMonth()}>
+                <div className="mini-nav">
+                    <h2 className="mini-nav-prev" onClick={() => props.prevMonth()}>
                         &#60;
                     </h2>
-                    <h2
-                        className='mini-nav-next'
-                        onClick={() => props.nextMonth()}>
+                    <h2 className="mini-nav-next" onClick={() => props.nextMonth()}>
                         &#62;
                     </h2>
                 </div>
             </div>
-            <Line
-                annim={props.annim}
-                month={props.month}
-                year={props.year}
-                sele={props.isSele}
-                nowWeek={props.week}
-                offset={0}
-                premier={true}
-                dernier={false}
-            />
-            <Line
-                annim={props.annim}
-                month={props.month}
-                year={props.year}
-                sele={props.isSele}
-                nowWeek={props.week}
-                offset={7}
-                premier={false}
-                dernier={false}
-            />
-            <Line
-                annim={props.annim}
-                month={props.month}
-                year={props.year}
-                sele={props.isSele}
-                nowWeek={props.week}
-                offset={14}
-                premier={false}
-                dernier={false}
-            />
-            <Line
-                annim={props.annim}
-                month={props.month}
-                year={props.year}
-                sele={props.isSele}
-                nowWeek={props.week}
-                offset={21}
-                premier={false}
-                dernier={false}
-            />
-            <Line
-                annim={props.annim}
-                month={props.month}
-                year={props.year}
-                sele={props.isSele}
-                nowWeek={props.week}
-                offset={28}
-                premier={false}
-                dernier={true}
-            />
+            <Line annim={props.annim} month={props.month} year={props.year} sele={props.isSele} nowWeek={props.week} offset={0} premier={true} dernier={false} />
+            <Line annim={props.annim} month={props.month} year={props.year} sele={props.isSele} nowWeek={props.week} offset={7} premier={false} dernier={false} />
+            <Line annim={props.annim} month={props.month} year={props.year} sele={props.isSele} nowWeek={props.week} offset={14} premier={false} dernier={false} />
+            <Line annim={props.annim} month={props.month} year={props.year} sele={props.isSele} nowWeek={props.week} offset={21} premier={false} dernier={false} />
+            <Line annim={props.annim} month={props.month} year={props.year} sele={props.isSele} nowWeek={props.week} offset={28} premier={false} dernier={true} />
         </div>
     );
 };
