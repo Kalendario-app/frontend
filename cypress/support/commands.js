29bb9ff15f5ee7aaf030a85ee610b6cee8a254e3
samuelvitry <samuel.vitry@orange.fr>
2022-02-16 23:04:59 +0100

    optimisation

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,5 @@
 import "@4tw/cypress-drag-drop";
+import { length } from "file-loader";
 Cypress.Commands.add("addBlankEvent", () => {
     cy.get(".monthly-top-button button").click();
     cy.get("input[placeholder='Event name']").type("test event");
@@ -17,19 +18,27 @@ Cypress.Commands.add("deleteEvent", () => {
     cy.get("@dayCard").should("not.contain", "test event");
 });
 Cypress.Commands.add("login", () => {
-    cy.visit("127.0.0.1:3000/login");
-    cy.get("input[type=email]").type(Cypress.env("email"));
-    cy.get("input[type=password]").type(Cypress.env("password"));
-    cy.intercept("https://api.kalendario.app/api/login?mdp=*").as("login");
+    cy.visit("127.0.0.1:3000/calendar");
+    cy.get("body").then(($body) => {
+        if ($body.find("header").length === 0) {
+            cy.visit("127.0.0.1:3000/login");
+            cy.get("input[type=email]").type(Cypress.env("email"));
+            cy.get("input[type=password]").type(Cypress.env("password"));
+            cy.intercept("https://api.kalendario.app/api/login?mdp=*").as("login");
+            cy.intercept("https://api.kalendario.app/api/").as("api");
+            cy.get(".login-submit button").click();
+            cy.wait("@api");
+        }
+    });
     cy.intercept("https://api.kalendario.app/api/").as("api");
-    cy.get(".login-submit button").click();
+    cy.visit("127.0.0.1:3000/calendar");
     cy.wait("@api");
-    cy.get("body").then((body) => {
-        if (body.find(".code-in-line").length > 0) {
+    cy.get("body").then(($body) => {
+        if ($body.find(".code-popup").length !== 0) {
             cy.get(".input-contained").type(Cypress.env("code"));
             cy.get(".code-in-line button").click();
         }
-        if (body.find(".verif-cross").length > 0) {
+        if ($body.find(".verif-cross").length !== 0) {
             cy.get(".verif-cross").click();
         }
     });
