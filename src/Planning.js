1dd5a32bfaf2c435d77ec21a189d40e4554c7d9b
samuelvitry <samuel.vitry@orange.fr>
2022-02-20 22:26:36 +0100

    planning logic final

--- src/Planning.js
+++ src/Planning.js
@@ -7,6 +7,15 @@ export const Planning = (props) => {
     const [, setHeight] = useState(window.innerHeight);
 
     const monthConv = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const dayConv = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+    };
 
     const updateWidthAndHeight = () => {
         setHeight(window.innerHeight);
@@ -81,10 +90,34 @@ export const Planning = (props) => {
         return a["start_date"] - b["start_date"];
     });
 
-    let stockage = [
+    let stockageTest = [
         ["January 2022", ["Monday 18", [props.eventList[0], props.eventList[1]]]],
         ["February 2022", ["Monday 18", [props.eventList[0]]]],
     ];
+
+    let stockage = [];
+
+    let oldM = null;
+    let oldD = null;
+    for (let i = 0; i < events.length; i++) {
+        let m = new Date(events[i]["start_date"] * 1000).getMonth();
+        let d = new Date(events[i]["start_date"] * 1000).getDate();
+        if (oldM === m && oldD === d) {
+            stockage[stockage.length - 1][stockage[stockage.length - 1].length - 1][1].push(events[i]);
+        } else if (oldM === m) {
+            stockage[stockage.length - 1].push([
+                dayConv[new Date(events[i]["start_date"] * 1000).getDay()] + " " + new Date(events[i]["start_date"] * 1000).getDate(),
+                [events[i]],
+            ]);
+        } else {
+            stockage.push([
+                monthConv[m] + " " + new Date(events[i]["start_date"] * 1000).getFullYear(),
+                [dayConv[new Date(events[i]["start_date"] * 1000).getDay()] + " " + new Date(events[i]["start_date"] * 1000).getDate(), [events[i]]],
+            ]);
+        }
+        oldM = m;
+        oldD = d;
+    }
 
     return (
         <div style={{ height: height }} className="planning">
