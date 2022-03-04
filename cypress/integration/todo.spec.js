e77aaea2ddd4b63fe96abfb62a88ba9af2a9bd87
samuelvitry <samuel.vitry@orange.fr>
2022-03-04 11:50:10 +0100

    time input is working

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -3,7 +3,7 @@ it("todo work", () => {
     cy.get(".todo-container > .calendar-select-top > #calendar-add").click();
     cy.get(".input-open").type("test");
     cy.get(".input-open").type("{enter}");
-    cy.get(".button-empty").click();
+    cy.get(".input-open").type("{esc}");
     cy.get(".todo-item").each(($el) => {
         cy.wait(500);
         cy.get(".todo-item").eq(0).click();
@@ -16,6 +16,6 @@ it("todo work", () => {
     cy.get(".todo-container > .calendar-select-top > #calendar-add").click();
     cy.get(".input-open").type("test");
     cy.get(".input-open").type("{enter}");
-    cy.get(".button-empty").click();
+    cy.get(".input-open").type("{esc}");
     cy.get(".todo-list").should("contain", "test");
 });
--- src/TimeInput.js
+++ src/TimeInput.js
@@ -7,6 +7,23 @@ export const TimeInput = (props) => {
     const [fullDay, setFullDay] = useState(props.full ? true : false);
     const [date, setDate] = useState(props.date !== undefined ? new Date(props.date) : new Date());
 
+    const monthConv = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    function getJour(nbr) {
+        var offsetbeggin = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
+        var day = new Date(date.getFullYear(), date.getMonth(), nbr - offsetbeggin);
+        return day;
+    }
+
+    var maxNbr = 4;
+    if (getJour(29).getMonth() === date.getMonth()) {
+        maxNbr = 5;
+    }
+    if (getJour(36).getMonth() === date.getMonth()) {
+        maxNbr = 6;
+    }
+    var arr = [...Array(maxNbr * 7).keys()].map((x) => x + 1);
+
     return (
         <>
             <div className="time-input" style={{ borderColor: props.color }} onClick={() => setIsPop(true)}>
@@ -16,126 +33,168 @@ export const TimeInput = (props) => {
             {isPop ? (
                 <div className="add-container time-input-cont" onClick={() => setIsPop(false)}>
                     <div className="add-popup time-input-pop" onClick={(e) => e.stopPropagation()}>
-                        <div className="time-in-cont">
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setDate(date.getDate() + 1)));
-                                }}>
-                                <i class="fas fa-chevron-up"></i>
-                            </button>
-                            <input
-                                style={{ width: "2em" }}
-                                type="number"
-                                value={date.getDate()}
-                                onChange={(e) => {
-                                    setDate(new Date(date.setDate(e.target.value)));
-                                }}
-                                min={1}
-                                max={new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate()}
-                            />
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setDate(date.getDate() - 1)));
-                                }}>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
+                        <div className="time-input-nbr">
+                            <div className="time-in-cont">
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setDate(date.getDate() + 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-up"></i>
+                                </button>
+                                <input
+                                    style={{ width: "2em" }}
+                                    type="number"
+                                    value={date.getDate()}
+                                    onChange={(e) => {
+                                        setDate(new Date(date.setDate(e.target.value)));
+                                    }}
+                                    min={1}
+                                    max={new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate()}
+                                />
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setDate(date.getDate() - 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-down"></i>
+                                </button>
+                            </div>
+                            <p>/</p>
+                            <div className="time-in-cont">
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setMonth(date.getMonth() + 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-up"></i>
+                                </button>
+                                <input
+                                    style={{ width: "2em" }}
+                                    type="number"
+                                    value={date.getMonth() + 1}
+                                    onChange={(e) => {
+                                        setDate(new Date(date.setMonth(e.target.value - 1)));
+                                    }}
+                                    min={1}
+                                    max={12}
+                                />
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setMonth(date.getMonth() - 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-down"></i>
+                                </button>
+                            </div>
+                            <p>/</p>
+                            <div className="time-in-cont">
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setFullYear(date.getFullYear() + 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-up"></i>
+                                </button>
+                                <input
+                                    style={{ width: "4em", marginRight: ".8em" }}
+                                    type="number"
+                                    value={date.getFullYear()}
+                                    onChange={(e) => {
+                                        setDate(new Date(date.setFullYear(e.target.value)));
+                                    }}
+                                />
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setFullYear(date.getFullYear() - 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-down"></i>
+                                </button>
+                            </div>
+                            <div className="time-in-cont">
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setHours(date.getHours() + 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-up"></i>
+                                </button>
+                                <input
+                                    style={{ width: "2em" }}
+                                    type="number"
+                                    value={date.getHours()}
+                                    onChange={(e) => {
+                                        setDate(new Date(date.setHours(e.target.value)));
+                                    }}
+                                    min={0}
+                                    max={23}
+                                />
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setHours(date.getHours() - 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-down"></i>
+                                </button>
+                            </div>
+                            <p>:</p>
+                            <div className="time-in-cont">
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setMinutes(date.getMinutes() + 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-up"></i>
+                                </button>
+                                <input
+                                    style={{ width: "2em" }}
+                                    type="number"
+                                    value={date.getMinutes() > 10 ? date.getMinutes() : "0" + date.getMinutes()}
+                                    onChange={(e) => {
+                                        setDate(new Date(date.setMinutes(e.target.value)));
+                                    }}
+                                    min={0}
+                                    max={59}
+                                />
+                                <button
+                                    onClick={() => {
+                                        setDate(new Date(new Date(date).setMinutes(date.getMinutes() - 1)));
+                                    }}>
+                                    <i className="fas fa-chevron-down"></i>
+                                </button>
+                            </div>
                         </div>
-                        <p>/</p>
-                        <div className="time-in-cont">
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setMonth(date.getMonth() + 1)));
-                                }}>
-                                <i class="fas fa-chevron-up"></i>
-                            </button>
-                            <input
-                                style={{ width: "2em" }}
-                                type="number"
-                                value={date.getMonth() + 1}
-                                onChange={(e) => {
-                                    setDate(new Date(date.setMonth(e.target.value - 1)));
-                                }}
-                                min={1}
-                                max={12}
-                            />
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setMonth(date.getMonth() - 1)));
-                                }}>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
+                        <div className="time-input-cal">
+                            <div className="time-cal-head">
+                                <h2 className="time-cal-prev time-cal-nav" onClick={() => setDate(new Date(new Date(date).setMonth(date.getMonth() - 1)))}>
+                                    &#60;
+                                </h2>
+                                <h2>{monthConv[date.getMonth()] + " " + date.getFullYear()}</h2>
+                                <h2 className="time-cal-next time-cal-nav" onClick={() => setDate(new Date(new Date(date).setMonth(date.getMonth() + 1)))}>
+                                    &#62;
+                                </h2>
+                            </div>
+                            <div className="time-cal-body">
+                                {arr.map((x, y) => {
+                                    return (
+                                        <div className="time-cal-day" key={y}>
+                                            <h2
+                                                style={{
+                                                    backgroundColor:
+                                                        getJour(x).getDate() === date.getDate() && getJour(x).getMonth() === date.getMonth() ? "#3581b873" : null,
+                                                }}
+                                                onClick={(e) => {
+                                                    setDate(
+                                                        new Date(
+                                                            getJour(x).getFullYear(),
+                                                            getJour(x).getMonth(),
+                                                            getJour(x).getDate(),
+                                                            date.getHours(),
+                                                            date.getMinutes()
+                                                        )
+                                                    );
+                                                }}>
+                                                {getJour(x).getDate()}
+                                            </h2>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
-                        <p>/</p>
-                        <div className="time-in-cont">
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setFullYear(date.getFullYear() + 1)));
-                                }}>
-                                <i class="fas fa-chevron-up"></i>
-                            </button>
-                            <input
-                                style={{ width: "4em", marginRight: ".8em" }}
-                                type="number"
-                                value={date.getFullYear()}
-                                onChange={(e) => {
-                                    setDate(new Date(date.setFullYear(e.target.value)));
-                                }}
-                            />
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setFullYear(date.getFullYear() - 1)));
-                                }}>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
-                        </div>
-                        <div className="time-in-cont">
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setHours(date.getHours() + 1)));
-                                }}>
-                                <i class="fas fa-chevron-up"></i>
-                            </button>
-                            <input
-                                style={{ width: "2em" }}
-                                type="number"
-                                value={date.getHours()}
-                                onChange={(e) => {
-                                    setDate(new Date(date.setHours(e.target.value)));
-                                }}
-                                min={0}
-                                max={23}
-                            />
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setHours(date.getHours() - 1)));
-                                }}>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
-                        </div>
-                        <p>:</p>
-                        <div className="time-in-cont">
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setMinutes(date.getMinutes() + 1)));
-                                }}>
-                                <i class="fas fa-chevron-up"></i>
-                            </button>
-                            <input
-                                style={{ width: "2em" }}
-                                type="number"
-                                value={date.getMinutes() > 10 ? date.getMinutes() : "0" + date.getMinutes()}
-                                onChange={(e) => {
-                                    setDate(new Date(date.setMinutes(e.target.value)));
-                                }}
-                                min={0}
-                                max={59}
-                            />
-                            <button
-                                onClick={() => {
-                                    setDate(new Date(new Date(date).setMinutes(date.getMinutes() - 1)));
-                                }}>
-                                <i class="fas fa-chevron-down"></i>
-                            </button>
+                        <div className="time-cal-foot">
+                            <Button onClick={() => setIsPop(false)} txt="Ok" />
                         </div>
                     </div>
                 </div>
